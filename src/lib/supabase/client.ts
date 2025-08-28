ac139901b6ea7b38ee8233782df7a2f112cb36ae
baygonEldiabate <baygon98@outlook.fr>
2025-08-28 23:53:23 +0000

    modified client.ts in supabase

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,9 +3,16 @@ import { createBrowserClient } from '@supabase/ssr'
 import { Database } from '@/types/supabase' // Assurez-vous que ce chemin est correct
 
 export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL and Anon Key must be defined in your environment variables');
+  }
+
   // Les variables d'environnement sont lues directement ici car ce code s'exécute dans le navigateur
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    supabaseUrl,
+    supabaseAnonKey
   )
 }
