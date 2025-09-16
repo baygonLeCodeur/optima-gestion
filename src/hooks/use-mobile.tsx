2957782598c37e49f9561c13613f00296c68e97d
baygonEldiabate <baygon98@outlook.fr>
2025-09-16 08:52:26 +0000

    msg

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,32 +1,24 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useIsClient } from './use-is-client'; // Importer le hook
+import { useIsClient } from './use-is-client';
 
 const useMobile = (query: string = '(max-width: 768px)') => {
-  const isClient = useIsClient(); // Utiliser le hook pour vérifier si on est côté client
+  const isClient = useIsClient();
+  const [isMobile, setIsMobile] = useState(false);
 
-  // Retourner `false` (ou une valeur par défaut) côté serveur
-  if (!isClient) {
-    return false;
-  }
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [isMobile, setIsMobile] = useState(
-    () => window.matchMedia(query).matches
-  );
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    const mediaQuery = window.matchMedia(query);
-    const handleChange = () => setIsMobile(mediaQuery.matches);
+    if (isClient) {
+      const mediaQuery = window.matchMedia(query);
+      const handleChange = () => setIsMobile(mediaQuery.matches);
+      
+      // Set the initial value correctly after hydration
+      handleChange();
 
-    mediaQuery.addEventListener('change', handleChange);
-    
-    // Pas besoin d'appeler handleChange ici car l'état initial est déjà correct
-    
-    return () => mediaQuery.removeEventListener('change', handleChange);
-  }, [query]);
+      mediaQuery.addEventListener('change', handleChange);
+      return () => mediaQuery.removeEventListener('change', handleChange);
+    }
+  }, [isClient, query]);
 
   return isMobile;
 };
